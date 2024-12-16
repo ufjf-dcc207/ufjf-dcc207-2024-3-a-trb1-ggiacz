9d5d18bc2579354ea95e18249ba56296b1de69e7
Gabriel Giacomo <gabrielgiacomopaes@gmail.com>
2024-12-16 15:17:21 -0300

    add animacao fade

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <RouterView />
+  <Transition name="route-fade">
+    <RouterView />
+  </Transition>
 </template>
 
 <script setup>
@@ -32,5 +34,20 @@ body,
   padding: 15px;
   text-align: center;
   transition: transform 0.1s ease-in-out, border-color 0.1s;
+}
+
+.route-fade-enter-active,
+.route-fade-leave-active {
+  transition: opacity 0.5s ease, transform 0.5s ease;
+}
+
+.route-fade-enter-from {
+  opacity: 0;
+  transform: translateY(20px);
+}
+
+.route-fade-leave-to {
+  opacity: 0;
+  transform: translateY(-20px);
 }
 </style>
