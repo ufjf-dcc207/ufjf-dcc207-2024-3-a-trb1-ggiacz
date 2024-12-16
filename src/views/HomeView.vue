5552f5f8786186f4ba8834778366fc53e550890b
Gabriel Giacomo <gabrielgiacomopaes@gmail.com>
2024-12-16 15:11:30 -0300

    cria home

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script setup>
+import Navbar from '../components/Navbar.vue';
+import Logo from '@/components/Logo.vue';
+
+</script>
+
+<style scoped>
+
+
+</style>
