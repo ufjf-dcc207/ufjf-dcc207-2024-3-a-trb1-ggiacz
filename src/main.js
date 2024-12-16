b958695f7d3a3164bf088c457d9e209d553c9ce0
Gabriel Giacomo <gabrielgiacomopaes@gmail.com>
2024-12-16 15:19:52 -0300

    configura icones

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import '@fortawesome/fontawesome-free/css/all.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faGear);
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(createPinia())
 app.use(router)
 
