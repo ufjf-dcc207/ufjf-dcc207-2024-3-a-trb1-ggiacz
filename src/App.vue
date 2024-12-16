171600c7179605fd5a30a56adf9b536baf049f83
Gabriel Giacomo <gabrielgiacomopaes@gmail.com>
2024-12-16 15:12:42 -0300

    estilo global

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,31 @@
 import { RouterView } from 'vue-router'
 </script>
 
-<style scoped>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: bold;
+  color: white;
+}
+
+html,
+body,
+#app {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+}
+
+.card {
+  background-color: black;
+  border: 2px solid white;
+  padding: 15px;
+  text-align: center;
+  transition: transform 0.1s ease-in-out, border-color 0.1s;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,21 @@
 <template>
-  <div>
-  </div>
-</template>
-
-<script setup>
-import Navbar from '../components/Navbar.vue';
-import Logo from '@/components/Logo.vue';
-
-</script>
-
-<style scoped>
-
-
-</style>
+    <div>
+      <Logo class="home-logo"/>
+      <Navbar/>
+    </div>
+  </template>
+  
+  <script setup>
+  import Navbar from '../components/Navbar.vue';
+  import Logo from '@/components/Logo.vue';
+  
+  </script>
+  
+  <style scoped>
+  .home-logo {
+    scale: 1.45;
+    margin-bottom: 75px;
+  }
+  
+  </style>
+  
